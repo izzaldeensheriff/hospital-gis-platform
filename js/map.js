e80f2b91dcae98c160fa4dfc77d4e2abe035356c
Izzaldeen SHERIFF <isheriff@cege0043-10.cs.ucl.ac.uk>
2026-01-28 10:28:55 +0000

    variable to store the leaflet map so that we can get hold of the map and make changes via code

---
+++ js/map.js
@@ -0,0 +1,12 @@
+/**
+ * variable to store the leaflet map so that we can get hold of the map and make changes via code
+ */
+let mymap; // stores the leaflet map
+
+
+
+
+/**
+ * variable to store the leaflet layer control so that we can get hold of the map and make changes via code
+ */
+let layerControl; // the leaflet layer control
