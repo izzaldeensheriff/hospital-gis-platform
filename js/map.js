8d502527e312e11b2e4e73d826860f61b6227340
Izzaldeen SHERIFF <isheriff@cege0043-10.cs.ucl.ac.uk>
2026-01-31 22:01:18 +0000

    updated the map script file with two variables, one to store the leaflet map, and the other to control the leaflet layer

---
+++ js/map.js
@@ -0,0 +1,13 @@
+
+/**
+ * variable to store the leaflet map so that we can get hold of the map and make changes via code
+ */
+let mymap; // stores the leaflet map
+
+
+
+
+/**
+ * variable to store the leaflet layer control so that we can get hold of the map and make changes via code
+ */
+let layerControl; // the leaflet layer control
