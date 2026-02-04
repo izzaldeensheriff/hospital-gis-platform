c6e8e1e7ce9d2c1d8280838fa35714cb49ba5a30
Izzaldeen SHERIFF <isheriff@cege0043-10.cs.ucl.ac.uk>
2026-02-04 22:32:09 +0000

    debugged the code code to load the bus stops data into the showBusStopsStyled function to visualise the bus stops points depending on their magnitude

--- js/busstops.js
+++ js/busstops.js
@@ -37,6 +37,97 @@ let busStopsLayer;
  function showBusStopsStyled() 
 
  { 
+      let testMarkerRed = L.AwesomeMarkers.icon({
+            icon: 'play',
+            markerColor: 'red'
+        });
+      let testMarkerGray = L.AwesomeMarkers.icon({
+            icon: 'play',
+            markerColor: 'gray'
+        });
+      
+        let testMarkerPink = L.AwesomeMarkers.icon({
+            icon: 'play',
+            markerColor: 'pink'
+        });
+
+        let testMarkerBlue = L.AwesomeMarkers.icon({
+            icon: 'play',
+            markerColor: 'blue'
+        });
+
+        let testMarkerPurple = L.AwesomeMarkers.icon({
+            icon: 'play',
+            markerColor: 'purple'
+        });
+        let testMarkerGreen = L.AwesomeMarkers.icon({
+            icon: 'play',
+            markerColor: 'green'
+        });
+
+        let testMarkerBlack = L.AwesomeMarkers.icon({
+            icon: 'play',
+            markerColor: 'black'
+        });
+
+        let testMarkerYellow = L.AwesomeMarkers.icon({
+            icon: 'play',
+            markerColor: 'yellow'
+        });
+
+        let testMarkerOrange = L.AwesomeMarkers.icon({
+            icon: 'play',
+            markerColor: 'orange'
+        });
+
+ {// as we are hosting the data on our server, we dono't need to provide the full https:// ... detail
+	let layerURL = "./data/busstops.geojson";
+	 $.ajax({url: layerURL, crossDomain: true,success: function(result){
+			// load the geoJSON layer
+    		busStopsLayer = L.geoJson(result,
+        		{
+            	// use point to layer to create the points
+            	pointToLayer: function (feature, latlng)
+            	{if (feature.properties.LOCALITY == 'Saltash Passage') {
+	                     return L.marker(latlng, {icon: testMarkerGray }).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+	                  }
+	                  if (feature.properties.LOCALITY == 'Barne Barton') {
+	                     return L.marker(latlng, {icon:testMarkerOrange}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+	                  }
+	                  if (feature.properties.LOCALITY == 'St Budeaux') {
+	                     return L.marker(latlng, {icon:testMarkerPurple}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+	                  }
+	                  if (feature.properties.LOCALITY == 'Stoke') {
+	                     return L.marker(latlng, {icon:testMarkerPink}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+	                  }
+	                  if (feature.properties.LOCALITY == 'Ham') {
+	                     return L.marker(latlng, {icon:testMarkerBlue}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+	                  }
+					 if (feature.properties.LOCALITY == 'Plympton') {
+	                     return L.marker(latlng, {icon:testMarkerGreen}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+	                  }
+
+	                  if (feature.properties.LOCALITY == 'Efford') {
+	                     return L.marker(latlng, {icon:testMarkerYellow}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+	                  }
+	                  else {
+	                    return L.marker(latlng, {icon:testMarkerBlack}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+	                  }
+		                return L.marker(latlng).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+           		},  // end of point to layer
+        }).addTo(mymap);
+
+    	// zoom to the layer
+    	mymap.fitBounds(busStopsLayer.getBounds());
+
+		} // end of the inner function
+	}); // end of the ajax request
+
+ 	
+ }
+
+
+	                  
 
  	alert("showBusStopsStyled")
  	// body...
