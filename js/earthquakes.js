a34b2ef79619213cc6a208814dfa9b1d00450ea1
Izzaldeen SHERIFF <isheriff@cege0043-10.cs.ucl.ac.uk>
2026-02-04 10:56:43 +0000

    Adde a code to to create the icons to the earthquake points depending on their magnitude, and a code to load the earthquake data

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -2,7 +2,7 @@
 
 // a fucnction to show the earthqukae data pulled form the API
 
-function showEarthquakes()  {  
+function showEarthquakes()  {  //code to create the icons to the earthquake points depending on their magnitude//
 
 	let layerURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
 	 $.ajax({url: layerURL, crossDomain: true,success: function(result){
@@ -22,7 +22,45 @@ function showEarthquakes()  {
 	 alert(" showEarthquakes");
 }
 
-function showStyledEarthquakes() { alert(" showStyledEarthquakes ")
+function showStyledEarthquakes() 
+{ 
+	let testMarkerGreen = L.AwesomeMarkers.icon({
+        icon: 'play',
+        markerColor: 'green'
+    });
+    let testMarkerPink = L.AwesomeMarkers.icon({
+        icon: 'play',
+        markerColor: 'pink'
+    });
+    alert(" showStyledEarthquakes ")
+
+    
+//the code to load the earthquake data//
 
 
+	 let layerURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
+	 $.ajax({url: layerURL, crossDomain: true,success: function(result){
+			// load the geoJSON layer
+    		let earthquakeLayer = L.geoJson(result,
+        		{
+            	// use point to layer to create the points
+            	pointToLayer: function (feature, latlng){
+		              // look at the GeoJSON file - specifically at the properties - to see the earthquake magnitude and use a different marker depending on this value
+		              // also include a pop-up that shows the place value of the earthquakes
+            		  // we also just use the place in the pop up icon
+		              if (feature.properties.mag > 1.75) {
+		                 return L.marker(latlng, {icon:testMarkerGreen}).bindPopup("<b>"+feature.properties.place +"</b>");
+		              }
+		              else {
+		                // magnitude is 1.75 or less
+		                return L.marker(latlng, {icon:testMarkerPink}).bindPopup("<b>"+feature.properties.place +"</b>");;
+		              }
+           		},  // end of point to layer
+        }).addTo(mymap);
+
+    	// zoom to the layer
+    	mymap.fitBounds(earthquakeLayer.getBounds());
+
+		} // end of the inner function
+	}); // end of the ajax request
 }
