0ac659433008e0fa1086c819bcc1c69aa32746cb
Izzaldeen SHERIFF <isheriff@cege0043-10.cs.ucl.ac.uk>
2026-02-04 16:46:02 +0000

    Debugged the showBusStopsStyled fucntion

--- js/busstops.js
+++ js/busstops.js
@@ -36,79 +36,9 @@ let busStopsLayer;
 
  function showBusStopsStyled() 
 
- {
-      let testMarkerRed = L.AwesomeMarkers.icon({
-            icon: 'play',
-            markerColor: 'red'
-        });
-      let testMarkerGray = L.AwesomeMarkers.icon({
-            icon: 'play',
-            markerColor: 'gray'
-        });
-      
-        let testMarkerPink = L.AwesomeMarkers.icon({
-            icon: 'play',
-            markerColor: 'pink'
-        });
+ { 
 
-        let testMarkerBlue = L.AwesomeMarkers.icon({
-            icon: 'play',
-            markerColor: 'blue'
-        });
-
-        let testMarkerPurple = L.AwesomeMarkers.icon({
-            icon: 'play',
-            markerColor: 'purple'
-        });
-        let testMarkerGreen = L.AwesomeMarkers.icon({
-            icon: 'play',
-            markerColor: 'green'
-        });
-
-        let testMarkerBlack = L.AwesomeMarkers.icon({
-            icon: 'play',
-            markerColor: 'black'
-        });
-
-        let testMarkerYellow = L.AwesomeMarkers.icon({
-            icon: 'play',
-            markerColor: 'yellow'
-        });
-
-        let testMarkerOrange = L.AwesomeMarkers.icon({
-            icon: 'play',
-            markerColor: 'orange'
-        });
-
-
-        	                  // look at the GeoJSON file - specifically at the properties - to see the busstops type
-	                  if (feature.properties.LOCALITY == 'Saltash Passage') {
-	                     return L.marker(latlng, {icon: testMarkerGray }).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
-	                  }
-	                  if (feature.properties.LOCALITY == 'Barne Barton') {
-	                     return L.marker(latlng, {icon:testMarkerOrange}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
-	                  }
-	                  if (feature.properties.LOCALITY == 'St Budeaux') {
-	                     return L.marker(latlng, {icon:testMarkerPurple}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
-	                  }
-	                  if (feature.properties.LOCALITY == 'Stoke') {
-	                     return L.marker(latlng, {icon:testMarkerPink}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
-	                  }
-	                  if (feature.properties.LOCALITY == 'Ham') {
-	                     return L.marker(latlng, {icon:testMarkerBlue}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
-	                  }
-					 if (feature.properties.LOCALITY == 'Plympton') {
-	                     return L.marker(latlng, {icon:testMarkerGreen}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
-	                  }
-
-	                  if (feature.properties.LOCALITY == 'Efford') {
-	                     return L.marker(latlng, {icon:testMarkerYellow}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
-	                  }
-	                  else {
-	                    return L.marker(latlng, {icon:testMarkerBlack}).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');;
-	                  }
-
- 	alert(" showBusStopsStyled ")
+ 	alert("showBusStopsStyled")
  	// body...
  }
 
