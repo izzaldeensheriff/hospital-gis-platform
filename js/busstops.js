ed183497cd3b93aa1e7509f176871ef1d2d6c178
Izzaldeen SHERIFF <isheriff@cege0043-10.cs.ucl.ac.uk>
2026-02-04 16:26:58 +0000

    Added code to the bus stop script to visualise the bus stops on the map

--- js/busstops.js
+++ js/busstops.js
@@ -1,5 +1,35 @@
  "use strict"
- function showBusStops() {alert(" showBusStops")
+
+ /**
+ * busStopsLayer
+ * 
+ * a global variable to store the bus stops layer once it has been added to the map
+ * global as will need to be referened by the bus stops remove functionality
+ * 
+ */
+let busStopsLayer;
+
+ function showBusStops() 
+
+ {// as we are hosting the data on our server, we dono't need to provide the full https:// ... detail
+	let layerURL = "./data/busstops.geojson";
+	 $.ajax({url: layerURL, crossDomain: true,success: function(result){
+			// load the geoJSON layer
+    		busStopsLayer = L.geoJson(result,
+        		{
+            	// use point to layer to create the points
+            	pointToLayer: function (feature, latlng){
+		                return L.marker(latlng).bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+           		},  // end of point to layer
+        }).addTo(mymap);
+
+    	// zoom to the layer
+    	mymap.fitBounds(busStopsLayer.getBounds());
+
+		} // end of the inner function
+	}); // end of the ajax request
+
+ 	alert(" showBusStops")
  	// body...
  }
 
